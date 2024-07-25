ec42a6f053eb0a3a270070b4735964e2b65d1e29
sushmasusarla <168928295+sushmasusarla@users.noreply.github.com>
2024-07-25 12:53:33 +0530

    Create busmodels.ts

---
+++ busbookings/src/app/models/busmodels.ts
@@ -0,0 +1,24 @@
+export interface Busmodel {
+    bid:number;
+    bstartplace:"string";
+    bendplace:"string";
+    bstarttime:"string";
+    bendtime:"string";
+    bprice:number;
+    bseats:number;
+    bstatus:"string";
+    bbookedseats:number;
+    bcompany:"string";
+    bdeparturedate:"string";
+    bdeparturetime:"string";
+    breturndate:"string";
+    bimage:"string";
+    bavailability:"string";
+    blocation:"string";
+    bdistance:"string";
+    bcontact:number;
+    bwebsite:"string";
+    bdetails:"string"
+    
+}
+
